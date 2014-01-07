6f679bd71e501e53110524147cdc0ae02b65af07
wanglaoyao01 <wang_laoyao01@126.com>
2014-01-07 14:37:31 +0800

    Create

---
+++ Test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by TY on 14-1-7.
+//  Copyright (c) 2014年 王懿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by TY on 14-1-7.
+//  Copyright (c) 2014年 王懿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
